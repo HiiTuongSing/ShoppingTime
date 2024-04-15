02105766a263a2d3a85f1dafc9247d8c8a456caf
HiiTuongSing <117565834+HiiTuongSing@users.noreply.github.com>
2024-04-15 22:28:40 +0800

    Product create/view/edit/delete

--- routes/products.js
+++ routes/products.js
@@ -11,11 +11,72 @@ router.get("/", async (req, res) => {
   }
 });
 
+//create product
 router.get("/new", async (req, res) => {
   try {
-    res.render("products/new");
+    const product = new Product();
+    res.render("products/new", { product: product });
   } catch {
     res.redirect("/");
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const product = new Product({
+      name: req.body.name,
+      price: req.body.price,
+      stock: req.body.stock,
+    });
+    await product.save();
+    const products = await Product.find({});
+    res.render("products/index", { products: products });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+//delete product
+router.delete("/:id", async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    const products = await Product.find({});
+    res.render("products/index", { products: products });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+//show product
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.render("products/show", { product: product });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+//edit product
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.render("products/edit", { product: product });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    product.name = req.body.name;
+    product.price = req.body.price;
+    product.stock = req.body.stock;
+    await product.save();
+    res.render("products/show", { product: product });
+  } catch (err) {
+    console.error(err);
   }
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -5,12 +5,16 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+app.use(methodOverride("_method"));
 
 const indexRouter = require("./routes/index");
 const productsRouter = require("./routes/products");
