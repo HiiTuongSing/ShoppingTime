e5ca1b174e10c2c3fee4a80ba016276ff6e6821a
HiiTuongSing <117565834+HiiTuongSing@users.noreply.github.com>
2024-04-23 11:10:36 +0800

    Implemented increase/decrease product quantity in cart

--- routes/cart.js
+++ routes/cart.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user");
 const Product = require("../models/product");
 
+//show the user cart
 router.get("/", async (req, res) => {
   try {
     const user = await User.findById(req.user);
@@ -21,6 +22,7 @@ router.get("/", async (req, res) => {
   }
 });
 
+//remove the whole product from the cart
 router.delete("/:id", async (req, res) => {
   try {
     const user = await User.findById(req.user);
@@ -64,6 +66,55 @@ router.delete("/:id", async (req, res) => {
     const updatedUserCart = updatedUser.cart;
 
     res.render("carts/index", { userCart: updatedUserCart });
+  } catch (err) {
+    console.error(err);
+    res.redirect("/");
+  }
+});
+
+//increase or reduce product at the cart
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      console.log("Product not found in user's cart.");
+      res.redirect("/");
+      return;
+    }
+    res.render("carts/editCart", { product: product });
+  } catch (err) {
+    console.error(err);
+    res.redirect("/");
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    let user = await User.findById(req.user);
+    const quantity = req.body.quantity;
+
+    //make changes to cart
+    let updatedQuantity =
+      parseInt(req.body.quantity) +
+      parseInt(
+        user.cart.find((item) => item.productId.toString() == req.params.id)
+          .productQuantity
+      );
+
+    await User.findByIdAndUpdate(
+      req.user,
+      { $set: { "cart.$[elem].productQuantity": parseInt(updatedQuantity) } },
+      { arrayFilters: [{ "elem.productId": req.params.id }], new: true }
+    );
+
+    await user.save();
+
+    //Make changes to product database
+    product.stock -= quantity;
+    await product.save();
+    user = await User.findById(req.user);
+    res.render("carts/index", { userCart: user.cart });
   } catch (err) {
     console.error(err);
     res.redirect("/");
